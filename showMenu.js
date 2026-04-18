21113cba3fbecd02c5f6224204421ab6eade7f40
dfomby <dvash.fomby19@stu.bmcc.cuny.edu>
2026-04-18 01:39:56 -0400

    nav bar, menu and menu icon fix

--- showMenu.js
+++ showMenu.js
@@ -1,8 +1,11 @@
 function showMenu() {
   var menu = document.getElementById("top-nav");
+  var x = document.querySelector(".material-symbols-outlined");
   if (menu.style.display === "none" || menu.style.display === '') {
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
   }
+
+   x.classList.toggle("change");
 }
